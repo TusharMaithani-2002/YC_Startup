81d2033f7cd7b8bae693621445ec260275c90af3
Tushar Maithani <202051194@iiitvadodara.ac.in>
2024-11-01 18:09:13 +0530

    made changes in style and added hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,15 @@
-
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with Enterpreneurs
+        </h1>
+        {/* ! is used for overriding styles */}
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+      </section>
+    </>
   );
 }
