128a53b6bae5e0b5af4dce1b2b0c0a7a3de0e324
Tushar Maithani <202051194@iiitvadodara.ac.in>
2024-11-01 22:55:28 +0530

    added server side form in hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -7,8 +14,11 @@ export default function Home() {
         </h1>
         {/* ! is used for overriding styles */}
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
